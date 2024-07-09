d5880244dd9dc68d5b347d225fb4caffa5a2b306
yicr <yicr@users.noreply.github.com>
2024-07-09 21:10:42 +0900

    fix: jsii not extends Error, because new abstruct BaseError class created and extends Invalid error class defined

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,19 @@
 
-export class InvalidInternalDeifinitionParameterError extends Error {
+abstract class BaseError {
+  name: string;
+  message: string;
+
+  constructor(message: string) {
+    this.name = 'BaseError';
+    this.message = message;
+    Object.setPrototypeOf(this, Error.prototype);
+  }
+}
+
+export class InvalidInternalDefinitionParameterError extends BaseError {
   constructor(parameterName: string, message: string = `${parameterName} is invalid, because it is defined internally.`) {
     super(message);
-    this.name = 'InvalidInternalDeifinitionParameterError';
+    this.name = 'InvalidInternalDefinitionParameterError';
+    Object.setPrototypeOf(this, new.target.prototype);
   }
 }
