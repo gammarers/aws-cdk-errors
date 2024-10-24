7f3e4e697e09cb6bdc3628c0dee054aab66d8216
yicr <yicr@users.noreply.github.com>
2024-10-24 12:00:14 +0900

    feat!: Major version bump to 1.0.0 - Initial Stable Release

--- .projenrc.ts
+++ .projenrc.ts
@@ -9,13 +9,14 @@ const project = new cdk.JsiiProject({
   name: '@gammarers/aws-cdk-errors',
   projenrcTs: true,
   repositoryUrl: 'https://github.com/gammarers/aws-cdk-errors.git',
+  majorVersion: 1,
   npmAccess: javascript.NpmAccess.PUBLIC,
   releaseToNpm: true,
   depsUpgrade: true,
   depsUpgradeOptions: {
     workflowOptions: {
       labels: ['auto-approve', 'auto-merge'],
-      schedule: javascript.UpgradeDependenciesSchedule.expressions(['10 16 * * *']), // everyday
+      schedule: javascript.UpgradeDependenciesSchedule.expressions(['13 16 * * 6']),
     },
   },
   minNodeVersion: '18.0.0',
